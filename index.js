2784fe34d88797245c9edad2b60a08f25a4cd1be
Maneesh Kumar <maneeshprajapati2004@gmail.com>
2026-09-04 16:48:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Hello, World! from Maneeshmkp' });
+});
+
+
+app.get('/health', (req, res) => {
+    return res.status(200).json({ message: 'Server is healthy' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
